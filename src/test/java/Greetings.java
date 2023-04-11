a3058415e37403b7e5a6737d485105feb11105bd
ttursunbaikyzy <imissbishkek@gmail.com>
2023-04-10 21:50:06 -0500

    Member A added greeting in Italian

--- src/test/java/Greetings.java
+++ src/test/java/Greetings.java
@@ -1,5 +1,6 @@
 public class Greetings {
     public static void main(String[] args) {
         System.out.println("Hello class!");
+        System.out.println("Hello in Italian is: Ciao!");
     }
 }
